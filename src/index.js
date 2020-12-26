62df54fc3d5a91fa5eee4f9498de2adec60d5111
masahiro aoki <41312574+masahiro-aoki@users.noreply.github.com>
2020-12-26 13:48:23 +0900

    refactoring

--- src/index.js
+++ src/index.js
@@ -5,33 +5,37 @@ document.getElementById("add-btn").addEventListener("click", () => {
   if (item.value) {
     const list = document.createElement("li");
     const text = document.createTextNode(item.value);
-    const doneBtn = document.createElement("input");
-    const deleteBtn = document.createElement("input");
-    addBtnAttribute(doneBtn, "完了");
-    addBtnAttribute(deleteBtn, "削除");
-
     list.appendChild(text);
+
+    const doneBtn = document.createElement("input");
+    addBtnAttribute(doneBtn, "完了");
+    const deleteBtn = document.createElement("input");
+    addBtnAttribute(deleteBtn, "削除");
+    const restoreBtn = document.createElement("input");
+    addBtnAttribute(restoreBtn, "戻す");
+
     list.appendChild(doneBtn);
     list.appendChild(deleteBtn);
-
-    list.children[0].addEventListener("click", () => {
-      list.children[0].value = "戻す";
-      list.removeChild(list.children[1]);
-      const clone = list.cloneNode(true);
-      clone.children[0].addEventListener("click", () => {
-        document.getElementById("todo-list").appendChild(list);
-        list.children[0].value = "完了";
-        list.appendChild(deleteBtn);
-        clone.parentNode.removeChild(clone);
-      });
-      document.getElementById("done-list").appendChild(clone);
-      list.parentNode.removeChild(list);
-    });
-    list.children[1].addEventListener("click", () => {
-      list.parentNode.removeChild(list);
-    });
     document.getElementById("todo-list").appendChild(list);
     item.value = "";
+
+    doneBtn.addEventListener("click", () => {
+      document.getElementById("done-list").appendChild(list);
+      list.removeChild(doneBtn);
+      list.removeChild(deleteBtn);
+      list.appendChild(restoreBtn);
+    });
+
+    deleteBtn.addEventListener("click", () => {
+      list.parentNode.removeChild(list);
+    });
+
+    restoreBtn.addEventListener("click", () => {
+      document.getElementById("todo-list").appendChild(list);
+      list.removeChild(restoreBtn);
+      list.appendChild(doneBtn);
+      list.appendChild(deleteBtn);
+    });
   }
 });
 
